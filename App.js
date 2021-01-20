52a1ec69063d0638fcdeace6fb3dfc98b50f217a
c-champagne <cassielchampagne@gmail.com>
2021-01-19 21:53:35 -0500

    Added line by line explanations

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-class App extends React.Component {
+class App extends React.Component {  //class component
     constructor(props) {
         super(props);
-        let initialState = {
+        let initialState = {  //creating our initial state, which holds an array named "colors" in which each object will be the values for one of our colored boxes on the screen"
             colors: [
                 {hex: "#54c06c", isLocked: false},
                 {hex: "#e4d623", isLocked: false},
@@ -11,39 +11,39 @@ class App extends React.Component {
             ]
         }
 
-        this.state = initialState;
+        this.state = initialState;  //setting our state to our defined initialState from above
     }
 
-    toggleLock(i) {
-        let newColors = this.state.colors.map((color, index) => {
-            if (i === index) {
+    toggleLock(i) {  //method we are creating for which will be used to change the isLocked property that is stored in our state above 
+        let newColors = this.state.colors.map((color, index) => {  //using map to create a new state
+            if (i === index) { //looking through our colors array to only update the color box that was clicked on/selected
                 return {
-                    ...color,
-                    isLocked: !color.isLocked
+                    ...color, //spread operator to copy all of the properties of our state
+                    isLocked: !color.isLocked //flipping our boolean for isLocked. This sets it to the opposite of whatever its current value is.
                 }
             } else {
-                return color
+                return color //for boxes we didn't click on, simply returning them as they already were
             }
         })
 
-        this.setState({
-            colors: newColors
+        this.setState({ //updating our state with setState to reflect the new state of isLocked for the box we clicked on
+            colors: newColors 
         });
     }
 
-    randomize() {
-        let newColors = this.state.colors.map(color => {
-            if (!color.isLocked) {
+    randomize() {  //method we are creating to randomize the colors of the color boxes
+        let newColors = this.state.colors.map(color => {  //using map to create a new state
+            if (!color.isLocked) { //checking to see if color is not locked.  We don't want to change our locked boxes.
                 return {
-                    ...color,
-                    hex: '#' + Math.floor(Math.random() * 16777215).toString(16)
+                    ...color, //spread operator to copy all of the properties of our state
+                    hex: '#' + Math.floor(Math.random() * 16777215).toString(16) //updating our hex using some code that generates a random hex id.  Don't stress about writing this from scratch - Google it!
                 }
-            } else {
+            } else { //for boxes that were locked, we return the color they already were
                 return color
             }
         })
 
-        this.setState({
+        this.setState({ //updating our state with setState to reflect the new colors
             colors: newColors
         });
     }
@@ -51,13 +51,20 @@ class App extends React.Component {
     render() {
         return (
             <div>
+               {/*  Our Randomize Colors Button */}
                 <div className="text-center bg-dark fixed-top">
                     <button onClick={() => this.randomize()} className="btn btn-secondary m-1">RANDOMIZE COLORS</button>
                 </div>
+
+
+                {/* Container for our color boxes.  All colors boxes will be rendered as separate divs within this div*/}
                 <div className="w-100 d-flex" style={{height: "100vh"}}>
                     {
+                        //Using .map() to bring in a Color component for each object in our state(the colors array)
                         this.state.colors.map((color, i) => <Color color={color} key={i} toggle={() => this.toggleLock(i)}/>)
-                    }
+                        //Above, the color (from our state) of our current object within our array, is passed as a prop (prop.color) to our Color component
+                        //Passing our toggleLock() method as a prop (prop.toggle), so that we can call it when we click the button in our Color component
+                   }
                 </div>
             </div>
         )
--- Color.js
+++ Color.js
@@ -1,12 +1,18 @@
 const Color = props => {
-    
-    let { color, toggle } = props;
-    let { hex, isLocked } = color;
+    //Our props are set for each color box in the .map() method used in our render from App.js
+    let { color, toggle } = props; //defining props.color and props.toggle as color(contains our hex color and lock state) and toggle (contains our toggleLock() method)
+    let { hex, isLocked } = color; //color here is our props.color from above. Defining color.hex and color.isLocked as hex and isLocked
 
     return (
+        //Creating a div and setting the backgroundColor of our div to the hex value from color (props.color) above
         <div style={{ backgroundColor: hex }} className="w-100 d-flex flex-column align-items-center justify-content-center">
-            <h1>{hex}</h1>
+            <h1>{hex}</h1> {/* Setting the text of our box to our current hex value from color (props.color) */}
+            
+            {/* Our Lock/Unlock Button */}
             <button onClick={toggle} className={isLocked ? 'btn btn-dark' : 'btn btn-outline-dark'}>{isLocked ? 'UNLOCK' : 'LOCK'}</button>
+           {/*  This button calls our toggleLock() method from App.js */}
+           {/* Using ternary to set className for fun Bootstrap styling */}
+           {/* Using ternary to conditionally render the text for our button */}
         </div>
     )
 }
